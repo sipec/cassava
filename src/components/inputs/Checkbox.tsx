ceb1d84fe7d04aff824434a69c3460799bab6bcf
Sinclair Chen <abc.sinclair@gmail.com>
2025-04-19 10:15:41 +0800

    💄 Show indeterminate checkbox state

--- src/components/inputs/Checkbox.tsx
+++ src/components/inputs/Checkbox.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react'
+
 export const Checkbox = (props: {
  checked: true | false | null
  nullDefault?: true | false
@@ -5,9 +7,17 @@ export const Checkbox = (props: {
 }) => {
  const { checked, nullDefault, onChange } = props
 
+ const ref = useRef<HTMLInputElement>(null)
+ useEffect(() => {
+  if (ref.current) {
+   ref.current.indeterminate = checked === null
+  }
+ }, [checked])
+
  return (
   <div className="group grid h-full w-full">
    <input
+    ref={ref}
     type="checkbox"
     checked={!!checked}
     onChange={() => {
@@ -20,26 +30,26 @@ export const Checkbox = (props: {
       onChange(null)
      }
     }}
-    className="col-start-1 row-start-1 appearance-none outline-none checked:bg-amber-600 indeterminate:bg-yellow-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
+    className="col-start-1 row-start-1 cursor-pointer appearance-none outline-none checked:bg-amber-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
    />
    <svg
     fill="none"
     viewBox="0 0 14 14"
-    className="pointer-events-none col-start-1 row-start-1 size-6 self-center justify-self-center stroke-stone-500 group-has-[:checked]:stroke-white group-has-[:disabled]:stroke-gray-950/25 group-has-[:indeterminate]:stroke-white"
+    className="pointer-events-none col-start-1 row-start-1 size-6 self-center justify-self-center stroke-stone-500 group-has-[:checked]:stroke-white group-has-[:disabled]:stroke-gray-950/25"
    >
     <path
      d="M3 8L6 11L11 3.5"
      strokeWidth={2}
      strokeLinecap="round"
      strokeLinejoin="round"
-     className="opacity-0 group-has-[:checked]:opacity-100"
+     className="opacity-0 group-has-[:checked]:opacity-100 group-has-[:indeterminate]:opacity-0"
     />
     <path
      d="M3 7H11"
      strokeWidth={2}
      strokeLinecap="round"
      strokeLinejoin="round"
-     className="opacity-0 group-has-[:indeterminate]:opacity-100"
+     className="opacity-0 group-has-[:indeterminate]:opacity-50"
     />
     {/* X when unchecked */}
     <path
